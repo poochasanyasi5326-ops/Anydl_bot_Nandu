6b513f0caec0225b33eff6b4ba5ac9d7949ab27c
nsnandalal-stack <nsnandalal@gmail.com>
2025-12-28 20:28:25 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pip3 install -U yt-dlp
-# Starts Aria2 background process
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --seed-time=0 &
+pip3 install -U yt-dlp 
+# Start Aria2 with RPC enabled
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --seed-time=0 & 
+echo "🚀 Starting AnyDL Bot..."
 python3 -u main.py
