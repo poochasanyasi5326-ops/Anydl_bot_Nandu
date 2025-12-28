13e6994b492c49e02fa8823cc204ffe131e5d15b
nsnandalal-stack <nsnandalal@gmail.com>
2025-12-29 00:25:36 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install -U yt-dlp 
-# Added secret token for security
+# Start Aria2 with a secret token to fix the "Insecure" warning in logs
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --rpc-secret="AnyDL_Secret_123" --max-connection-per-server=10 --seed-time=0 & 
 echo "🚀 Starting AnyDL Bot..."
 python3 -u main.py
