5e4bc3b399b3a9ba10e63874b684ded1aa53cdf3
nsnandalal-stack <nsnandalal@gmail.com>
2025-12-28 20:28:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import asyncio
 from pyrogram import Client
 from aiohttp import web
 
-API_ID = int(os.environ.get("API_ID", "12345"))
-API_HASH = os.environ.get("API_HASH", "abcdef")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "123:abc")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def handle(request):
-    return web.Response(text="Bot is running! 🚀")
+    return web.Response(text="Bot is Active 🚀")
 
 async def start_web_server():
     app = web.Application()
     app.router.add_get("/", handle)
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", 8000)
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
     await site.start()
 
 class AnyDLBot(Client):
@@ -34,7 +34,6 @@ class AnyDLBot(Client):
             os.makedirs("downloads")
         await super().start()
         await start_web_server()
-        print("✅ Bot Started & Downloads folder created!")
 
 if __name__ == "__main__":
     AnyDLBot().run()
