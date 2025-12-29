7ebc1f126b2043d8dc920c796270bcb159515f51
nsnandalal-stack <nsnandalal@gmail.com>
2025-12-29 21:39:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 from pyrogram import Client
 from plugins.command import register
 
-BOT = Client(
+app = Client(
     "anydl",
     api_id=int(os.getenv("API_ID")),
     api_hash=os.getenv("API_HASH"),
     bot_token=os.getenv("BOT_TOKEN")
 )
 
-BOT.start()
-register(BOT)
-BOT.idle()
+app.start()
+register(app)
+app.idle()
