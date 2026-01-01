b60a785193569b900accae818c37fbc7d8226f0c
nsnandalal-stack <nsnandalal@gmail.com>
2026-01-01 23:42:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = 519459195  # ENFORCED
+
+DOWNLOAD_DIR = "/tmp/anydl"
